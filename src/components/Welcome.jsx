604902959f37ddba18c4e17dad828863b57b076e
Vmart <victor.martinezsegura@gmail.com>
2023-10-11 14:04:36 +0200

    small design changes to welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,21 +6,22 @@ import logo from "../assets/img/marvel-logo.png";
 export default function Welcome() {
   return (
     <>
-      <div className="negro">
+      <div className="backdrop">
         <div className="big-font">
           <img className="logo" src={logo} alt="marvel_logo" />
           MEMORY CARD GAME
         </div>
         <p className="mid-font blink_me">SELECT DIFFICULTY LEVEL</p>
         <div className="flex-level">
-          <a href="">
-            <img className="level-box" src={easy} alt="easy" />
+          <a className="caption" href="">
+            
+            <img className="level-box" src={easy} alt="easy" />4x4 
           </a>
-          <a href="">
-            <img className="level-box" src={medium} alt="medium" />
+          <a className="caption" href="">
+            <img className="level-box" src={medium} alt="medium" />4x6 
           </a>
-          <a href="">
-            <img className="level-box" src={hard} alt="hard" />
+          <a className="caption" href="">
+            <img className="level-box" src={hard} alt="hard" />5x6 
           </a>
         </div>
       </div>
